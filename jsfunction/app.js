20989d21d695b49b177e65c4c1e7d1c01efc9438
Aaron Felipe Isaacs <aaron.isaacs.ing@gmail.com>
2019-04-11 15:46:33 -0500

    Permisos CORS policy

--- jsfunction/app.js
+++ jsfunction/app.js
@@ -1,20 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
+const hostname = '192.168.1.180';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
 
     console.log(req.url);
     
-    if (req.method === 'GET' && req.url === '/moby-dick.epub') {
+    if (req.method === 'GET' && req.url === '/echo.epub') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/octet-stream');
         res.setHeader('Accept-Ranges', 'bytes');
-        res.setHeader('Server', 'Giitic')
+        res.setHeader('Server', 'Giitic');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     
-        fs.readFile('moby-dick.epub', (err, data) => {
+        fs.readFile('pre _ fer.epub', (err, data) => {
             if (err) throw err;
             console.log('Solicitud epub');
             res.end(data);
@@ -27,7 +31,6 @@ const server = http.createServer((req, res) => {
       }
     
 });
-
 
 server.listen(port, hostname, () => {
     console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
